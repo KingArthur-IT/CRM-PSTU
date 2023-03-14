74cc2fa9863900a4511c5a0791edc469d4fdfa69
Artem <67517973+KingArthur-IT@users.noreply.github.com>
2023-03-14 13:43:43 +0200

    init project

---
+++ client/src/layouts/default/Default.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-sheet width="300" class="mx-auto">
+      <v-form @submit.prevent>
+        <v-text-field
+          v-model="loginValue"
+          :rules="rules"
+          label="Логін"
+        ></v-text-field>
+        <v-btn type="submit" block class="mt-2">Відправити</v-btn>
+      </v-form>
+    </v-sheet>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        loginValue: '',
+        rules: [
+          value => {
+            if (value) return true
+            return 'Введіть ваш логін'
+          },
+        ],
+      }
+    }
+  }
+</script>
