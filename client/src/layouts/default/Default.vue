bbeb495d2d30ed2606b6e02f532bca7d2e030dc9
Artem <OstapT5@gmail.com>
2023-05-04 14:20:36 +0300

    firebase init

--- client/src/layouts/default/Default.vue
+++ client/src/layouts/default/Default.vue
@@ -3,10 +3,16 @@
     <v-sheet width="300" class="mx-auto">
       <v-form @submit.prevent>
         <v-text-field
-          v-model="loginValue"
+          v-model="emailValue"
           :rules="rules"
-          label="Логін"
+          label="Email"
         ></v-text-field>
+        <v-text-field
+          v-model="passwordValue"
+          :rules="rules"
+          label="Пароль"
+        ></v-text-field>
+        <p style="color: red" v-if="authError">Помилка авторизації. {{ authError }}</p>
         <v-btn type="submit" block class="mt-2" v-on:click="submit">Відправити</v-btn>
       </v-form>
     </v-sheet>
@@ -14,21 +20,37 @@
 </template>
 
 <script>
+  import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
+
   export default {
     data() {
       return {
-        loginValue: '',
+        emailValue: '',
+        passwordValue: '',
+        authError: '',
         rules: [
           value => {
             if (value) return true
-            return 'Введіть ваш логін'
+            return 'Це поле не може бути порожнім'
           },
         ],
       }
     },
     methods: {
-      submit() {
-        this.$router.push({ path: '/home' })
+      async submit() {
+        this.authError = ''
+        if (this.emailValue === '' || this.passwordValue === '') return
+
+        const auth = getAuth()
+        await signInWithEmailAndPassword(auth, this.emailValue, this.passwordValue)
+          .then( (response) => {
+            console.log(response);
+            this.$router.push({ path: '/home' })
+          } )
+          .catch( (error) => {
+            console.error(error)
+            this.authError = error.message
+          } )
       }
     }
   }
--- client/src/main.js
+++ client/src/main.js
@@ -13,6 +13,24 @@ import { createApp } from 'vue'
 // Plugins
 import { registerPlugins } from '@/plugins'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD9WjixxJoiqroACwZD9q4bMUoaPdtsfeM",
+  authDomain: "pstu-crm.firebaseapp.com",
+  projectId: "pstu-crm",
+  storageBucket: "pstu-crm.appspot.com",
+  messagingSenderId: "955447037172",
+  appId: "1:955447037172:web:5edd1daa58c14b8e3822a1"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 const app = createApp(App)
 
 registerPlugins(app)
